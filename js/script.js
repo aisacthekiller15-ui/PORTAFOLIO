451bb38911c27ec607d9b58a14dbf71d97f43d73
aisacthekiller15-ui <aisacthekiller15@gmail.com>
2025-10-03 00:12:09 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let nombres = "ANGGELO EFRAIN";
+let datos = ["Anggelo Efrain", "senati", "estudiante"];
+
+console.log(nombres);
+console.log(datos[2]);
+
+//esto es un comentario
+//let edad = prompt("ingresae tu edad: ");
+console.log(edad);
+
+//trabajando funciones
+function mostrarMensaje(){
+    alert("Hola mundo");
+}
+
+function sumar(){
+    let n1 = parseInt(prompt("Ingresa tu primer numero:"));
+    let n2 = parseInt(prompt("Ingresa tu segundo numero:"));
+    console.log("El resultado es: " +(n1 + n2));
+
+}
